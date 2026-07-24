069abaf923ac57a3b74d68fb6619a2bb43af7e0a
MarthaNava <navamartha.e@gmail.com>
2026-07-24 18:06:10 -0400

    Fix signin validation for missing credentials

--- controllers/users.js
+++ controllers/users.js
@@ -57,7 +57,13 @@ const createUser = (req, res) => {
 const login = (req, res) => {
   const { email, password } = req.body;
 
-  User.findUserByCredentials(email, password)
+  if (!email || !password) {
+    return res.status(BAD_REQUEST).send({
+      message: "Email and password are required.",
+    });
+  }
+
+  return User.findUserByCredentials(email, password)
     .then((user) => {
       const token = jwt.sign({ _id: user._id }, JWT_SECRET, {
         expiresIn: "7d",
